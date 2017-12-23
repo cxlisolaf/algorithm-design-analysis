baf06122aa8c60257d6470a3319f0ca53fb74c06
cxlisolaf <doramifa@gmail.com>
2017-12-22 17:49:59 -0700

    change

--- FFT.py
+++ FFT.py
@@ -20,16 +20,16 @@ def FFT(x):
     x = np.asarray(x, dtype=float)
     N = x.shape[0]
  
-    if N % 2 > 0:
-        raise ValueError("size of x must be a power of 2")
-    elif N <= 32:  
-        return DFT_slow(x)
-    else:
-        X_even = FFT(x[::2])
-        X_odd = FFT(x[1::2])
-        factor = np.exp(-2j * np.pi * np.arange(N) / N)
-        return np.concatenate([X_even + factor[:N / 2] * X_odd,
-                               X_even + factor[N / 2:] * X_odd])
+#    if N % 2 > 0:
+#        raise ValueError("size of x must be a power of 2")
+#    elif N <= 32:  
+#        return DFT_slow(x)
+#    else:
+    X_even = FFT(x[::2])
+    X_odd = FFT(x[1::2])
+    factor = np.exp(-2j * np.pi * np.arange(N) / N)
+    return np.concatenate([X_even + factor[:N / 2] * X_odd,
+                            X_even + factor[N / 2:] * X_odd])
      
 
 x = np.random.random(16)
